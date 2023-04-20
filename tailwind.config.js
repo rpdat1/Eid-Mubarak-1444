4a8f8f9a24a8ac4cb8fa73e4dfcafc3cb84d9a96
unknown <zahisalman2@gmail.com>
2023-04-20 14:52:46 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        almarai: ['Almarai'],
+        poppins: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
+
